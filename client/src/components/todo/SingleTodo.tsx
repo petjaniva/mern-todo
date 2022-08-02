ecb1db66e5fe36b02120dd72aac5d08596fe8bfe
Petja Hopo <petjaniv@gmail.com>
2022-08-02 12:58:51 +0300

    added info to title on singetodos. need to convert author id to author name

--- client/src/components/todo/SingleTodo.tsx
+++ client/src/components/todo/SingleTodo.tsx
@@ -1,40 +1,52 @@
 import * as React from "react";
 import { Todo } from "./TodoList";
 import axios from "axios";
-import {AiFillDelete} from "react-icons/ai";
+import { AiFillDelete } from "react-icons/ai";
 
 interface ITodoProps {
   todo: Todo;
 }
 
 const SingleTodo: React.FC<ITodoProps> = (props: ITodoProps) => {
-  const onDoneClick = (todo: Todo) =>{
+  const onDoneClick = (todo: Todo) => {
     const token = localStorage.getItem("token");
-    if (token)
-    {todo.isCompleted = !todo.isCompleted;
-    axios.put(`/todo/${todo._id}`, todo, { headers: { token: token } });}
-  }
-  const onDeleteClick = (todo: Todo) =>{
+    if (token) {
+      todo.isCompleted = !todo.isCompleted;
+      axios.put(`/todo/${todo._id}`, todo, { headers: { token: token } });
+    }
+  };
+  const onDeleteClick = (todo: Todo) => {
     const token = localStorage.getItem("token");
-    if (token)
-    {axios.delete(`/todo/${todo._id}`, { headers: { token: token } });}
-  }
-
-        return (<div
-            className="border border-grey-400 p-4 rounded-md flex items-center justify-end"
-            key={props.todo._id!.toString()}
-          >
-            <div className={"mr-auto" + (props.todo.isCompleted ? 'line-through' : '')}>
-            {props.todo.title}           </div>
-            <input
-              type="button"
-              className="py-2 px-4 bg-green-400 text-white rounded-md cursor-pointer"
-              value={(props.todo.isCompleted ? 'UNDONE' : 'DONE')}
-              onClick={() => onDoneClick(props.todo)}
-            />
-            <div className="px-2"></div>
-            <AiFillDelete className="w-6 h-6 cursor-pointer" onClick={() =>onDeleteClick(props.todo) }/>
-          </div>)
-    
-}
+    if (token) {
+      axios.delete(`/todo/${todo._id}`, { headers: { token: token } });
+    }
+  };
+  const author = props.todo.author;
+  const date = new Date(props.todo.date);
+  const dateOptions: Intl.DateTimeFormatOptions= { weekday: "long", year: "numeric", month: "long", day: "numeric", hour: "numeric", minute: "numeric" };
+  return (
+    <div
+      className="border border-grey-400 p-4 rounded-md flex items-center justify-end"
+      title={"date: " + date.toLocaleDateString("fi-FI", dateOptions) + " author: " + author}
+      key={props.todo._id!.toString()}
+    >
+      <div
+        className={"mr-auto" + (props.todo.isCompleted ? "line-through" : "")}
+      >
+        {props.todo.title}{" "}
+      </div>
+      <input
+        type="button"
+        className="py-2 px-4 bg-green-400 text-white rounded-md cursor-pointer"
+        value={props.todo.isCompleted ? "UNDONE" : "DONE"}
+        onClick={() => onDoneClick(props.todo)}
+      />
+      <div className="px-2"></div>
+      <AiFillDelete
+        className="w-6 h-6 cursor-pointer"
+        onClick={() => onDeleteClick(props.todo)}
+      />
+    </div>
+  );
+};
 export default SingleTodo;
--- client/src/components/todo/TodoList.tsx
+++ client/src/components/todo/TodoList.tsx
@@ -6,6 +6,8 @@ export interface Todo {
   title: string;
   isCompleted: boolean;
   _id: Types.ObjectId;
+  date: Date;
+  author: Types.ObjectId;
 }
 
 interface TodoListProps {
