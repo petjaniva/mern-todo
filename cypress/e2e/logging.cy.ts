6b18ae9e7e3943fd877c25b986021880338d8451
Petja Niva <petjaniv@gmail.com>
2022-09-02 16:08:16 +0300

    first test done

--- cypress/e2e/logging.cy.ts
+++ cypress/e2e/logging.cy.ts
@@ -1,7 +1,16 @@
 describe("empty spec", () => {
-  it("passes", () => {
+  it("signup &login", () => {
     cy.visit("http://localhost:3000/");
     cy.contains("Signup").click();
     cy.contains("username").should("exist");
+    cy.get("input").eq(0).type("ruurico@ruurico.org");
+    cy.get("input").eq(1).type("salasana");
+    cy.get("input").eq(2).type("salasana");
+    cy.get("button").click();
+    cy.contains("login").should("exist");
+    cy.get("input").eq(0).type("ruurico@ruurico.org");
+    cy.get("input").eq(1).type("salasana");
+    cy.get("button").click();
+    cy.contains("my todos").should("exist");
   });
 });
