43b95ddea423ff23ed44d980e653617689236479
Petja Niva <petjaniv@gmail.com>
2022-09-09 12:21:29 +0300

    creating orgtodos seems to work well now and the tests too

--- cypress/e2e/creatingTodo.cy.ts
+++ cypress/e2e/creatingTodo.cy.ts
@@ -9,11 +9,33 @@ describe("create a todo", () => {
     cy.get("input").eq(0).type("test todo");
     cy.get("input").eq(2).click();
     cy.contains("test todo").should("exist");
+    cy.get("input").eq(0).should("have.value", "");
+    cy.contains("logout").click();
   });
   it("create org todo", () => {
+    cy.visit("http://localhost:3000/");
+    cy.contains("login").should("exist");
+    cy.get("input").eq(0).type("ruurico@testorg.org");
+    cy.get("input").eq(1).type("salasana");
+    cy.get("button").click();
+    cy.contains("my todos").should("exist");
     cy.get("input").eq(0).type("test orgtodo");
     cy.get("input").eq(1).click();
     cy.get("input").eq(2).click();
+    cy.contains("test orgtodo").should("exist");
+    cy.contains("logout").click();
+  });
+  it("org todo shows on other user account", () => {
+    cy.visit("http://localhost:3000/");
+    const user = {
+      email: "tester@testorg.org",
+      password: "salasana",
+      orgCode: "testorg",
+    };
+    cy.get("input").eq(0).type(user.email);
+    cy.get("input").eq(1).type(user.password);
+    cy.get("button").click();
+    cy.contains("my todos").should("exist");
     cy.contains("test orgtodo").should("exist");
   });
 });
